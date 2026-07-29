b8bc9f4acfab3c2f2abad4f082fdad2b09b15697
rick <ricardostumm.dev@gmail.com>
2026-07-29 13:55:28 -0300

    renaming repo and site

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ import { defineConfig, fontProviders } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://rick-os.github.io',
-  base: '/rickos',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
