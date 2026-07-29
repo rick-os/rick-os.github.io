e6dceb69f93c8a81814bcba25d9585ae6641d19a
rick <ricardostumm.dev@gmail.com>
2026-07-29 13:42:27 -0300

    editing astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,8 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://rick-os.github.io',
+  base: '/rickos'
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
