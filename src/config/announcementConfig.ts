537c9a3656e386736b9e20ffb94827a27eb6e88e
rick <ricardostumm.dev@gmail.com>
2026-07-29 22:57:27 -0300

    BREAKING CHANGE: CHANGED THEME

---
+++ src/config/announcementConfig.ts
@@ -0,0 +1,14 @@
+import type { AnnouncementConfig } from "../types/config";
+
+// 公告栏配置
+export const announcementConfig: AnnouncementConfig = {
+	title: "", // 公告标题，填空使用i18n字符串Key.announcement
+	content: "Eu vim fazer um anúncio, shadow o ouriço é um filho da puta.", // 公告内容
+	closable: true, // 允许用户关闭公告
+	link: {
+		enable: true, // 启用链接
+		text: "Learn More", // 链接文本
+		url: "/about/", // 链接 URL
+		external: false, // 内部链接
+	},
+};
--- src/consts.ts
+++
@@ -1,5 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'rick.os';
-export const SITE_DESCRIPTION = 'Welcome to my life docs, here you will read articles about tech, developing and many other subjects.';
